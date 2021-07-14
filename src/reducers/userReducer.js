2e6d332ad4c0af7ab8b2380e03c8369254767ab4
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-14 00:14:04 -0500

    update userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,23 @@
 export default function userReducer(state = { users: []}, action) {
     switch (action.type) {
+        case 'FETCH_USERS':
+            return {
+                users: action.payload,
+        }
+
+        case 'START_UPDATE_USER':
+            return {
+                ...state,
+                requesting: true
+            } 
+
+        case 'UPDATE_USER':
+            return {
+                users: action.payload,
+                requesting: false
+            }
+
         default:
-            return state
+             return state
     }
 }
