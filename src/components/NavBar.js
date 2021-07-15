5fd06cbc39755b0bafdd1626f740a33d07588a72
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-15 15:04:04 -0500

    update styling of nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+
 
 const NavBar = () => {
+  return (
+    <div className="topnav">
+      <a className="logo" href="/">Danielle Ellis</a>
+      <div className="justify-content-end" id="navbarDefault"/>
+      <a className="active" href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#portolio">Portfolio</a>
+      <a href="#blog">Blog</a>
+    </div>
+  )
+}
+
+
+
+
+/* const NavBar = () => {
   return (
     <nav className="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
       <div className="container">
         <a className="navbar-brand" href="/">Danielle Ellis</a>
+
         <div className="justify-content-end" id="navbarDefault" >
-          <ul className="navbar-nav">
+          
+        <ul className="navbar-nav">
             <li className='nav-item'>
               <NavLink
                 className='nav-link'
@@ -53,6 +71,6 @@ const NavBar = () => {
       </div>
     </nav>
   )
-}
+} */
 
 export default NavBar;
