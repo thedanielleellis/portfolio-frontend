135395ef4e04868ab9e6366332e72ca4c2e7a2e9
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-15 23:15:55 -0500

    created project container

---
+++ src/containers/ProjectsContainer.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import ProjectList from '../components/projects/ProjectList'
+import ProjectDetails from '../components/projects/ProjectDetails'
+import { fetchProjects } from "../actions/fetchProjects";
+
+class ProjectsContainer extends Component {
+  componentDidMount() {
+    this.props.fetchProjects()
+  }
+
+  render() {
+    const { filteredProjects, stacks, selectedStackIds, loading, addFilter, removeFilter } = this.props
+    return (
+      <div id='projects-container' className='component'>
+        {
+          loading ?
+            <img src={Loading} alt='loading' />
+            :
+            < >
+              {/* define parameter name in route path - show up in match.params */}
+              {/* replace `component` prop w/ `render` prop so we can pass route info to `ProjectDetails` component*/}
+              <Route path='/projects/:projectId' render={routerProps => <ProjectDetails {...routerProps} projects={filteredProjects} />} />
+              <Route exact path='/projects' render={() =>
+                <div id='portfolio'>
+                  <h1>PORTFOLIO</h1>
+                  <h3>My Latest Projects</h3>
+                  <ProjectFilters stacks={stacks} selectedStackIds={selectedStackIds} addFilter={addFilter} removeFilter={removeFilter} />
+                  <ProjectList filteredProjects={filteredProjects} />
+                </div>
+              } />
+            </ >
+        }
+      </div >
+    )
+  }
+}
+
+// passing in the state from the Redux store
+// so we can access values in our stores as props
+const mapStateToProps = state => {
+  return {
+    stacks: state.projects.stacks,
+    selectedStackIds: state.projects.selectedStackIds,
+    filteredProjects: state.projects.filteredProjects,
+    loading: state.projects.loading
+  }
+}
+
+// gives us ability to dispatch new actions to our store directly from this component
+const mapDispatchToProps = dispatch => {
+  return {
+    // wrap actions in a dispatch & anonymous fn to pass them as callback props
+    // when called, dispstch fn will invoke reducer and result in new updated state
+    fetchStacks: () => dispatch(fetchStacks()),
+    fetchProjects: () => dispatch(fetchProjects()),
+    addFilter: stackId => dispatch(addFilter(stackId)),
+    removeFilter: stackId => dispatch(removeFilter(stackId))
+  }
+}
+
+// connect redux store to this component
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsContainer)
