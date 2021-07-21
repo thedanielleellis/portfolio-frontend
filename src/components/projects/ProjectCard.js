d202e144063a671e98a430f049aa6fc3f040a5f1
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-21 18:02:04 -0500

    add thumnail photos to projects tab

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 
 const ProjectCard = ({project}) => {
     
-    const fileName = project.image_url
+    const fileName = project.attributes.image_url
     const images = require.context('../../images', true)
+    let img = images('./' + fileName).default
 
     return (
         <div id='project-list'>
           <Link key={project.id} to={`/projects/${project.id}`}>
             <h4>{project.attributes.name}</h4>
           </Link>
-          <img className='project-thumbnail' alt='project thumbnail' src="https://i.imgur.com/g5iq8k9.png" />
+          <img className='project-thumbnail' alt='project thumbnail' src={img} />
         </div >
       );
 };
