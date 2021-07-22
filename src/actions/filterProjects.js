6f1fdda778c8c0b7afa5a4f460c40d7e8edcf18c
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-22 17:21:38 -0500

    created a filter action

---
+++ src/actions/filterProjects.js
@@ -0,0 +1,11 @@
+export const addFilter = () => {
+    return {
+      type: 'ADD_FILTER',
+    };
+  };
+  
+  export const removeFilter = () => {
+    return {
+      type: 'REMOVE_FILTER',
+    };
+  };
