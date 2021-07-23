e9ff5d9e7bdf25c1bd0dbbf98c7280a7a882c459
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-23 10:12:15 -0500

    updated reducers

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -1,10 +1,7 @@
 const projectsReducer = (state = {
-    allProjects: [],
-    filteredProjects: [], 
+    allProjects: [], 
     loading: false,
     }, action) => {
-        let filteredProjects = []
-
     switch (action.type) {
         case "LOADING_PROJECTS":
             return {
@@ -18,30 +15,10 @@ const projectsReducer = (state = {
                 allProjects: action.projects,
                 loading: false
             }
-
-        case 'ADD_FILTER':
-            return {
-                ...state,
-                filteredProjects: filteredProjects,
-            }
-          
-        case 'REMOVE_FILTER':
-                return {
-                  ...state,
-                  filteredProjects: filteredProjects,
-                }
-
         case 'ADD_COMMENT':
-            let index = state.filteredProjects.findIndex(project => project.id === action.comment.project_id);
-            let project = state.filteredProjects[index];
-
             return {
                 ...state,
-                filteredProjects: [
-                ...state.filteredProjects.slice(0, index),
-                { ...project, comments: project.comments.concat(action.comment) },
-                ...state.filteredProjects.slice(index + 1)
-                ]
+                comments: action.comment
             }
             
         default:
