0b684a252026cc20411f07d8c24a77c9ed20f877
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-23 10:15:57 -0500

    removed filter from project components

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ProjectCard from './ProjectCard'
 
-const ProjectList = ({ filteredProjects }) => {
-  const renderProjects = filteredProjects.map(project =>
+const ProjectList = ({allProjects}) => {
+  const renderProjects = allProjects.map(project => 
     <ProjectCard key={project.id} project={project} />
   )
   
 
   return (
     < >
-      {(filteredProjects.length > 0) ?
-        renderProjects
-        :
-        <i>There are no matching results.</i>
-      }
+       {renderProjects}    
+       :
+       <i>There are no matching results.</i>
+
     </ >
   )
 }
