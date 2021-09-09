3bf5a6634cf95ea6345d6d9372dc9be2a07ec48f
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-09 13:37:46 -0500

    update about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const About = () => {
+
   return (
     <div id='about' className='component'>
+
       <div className='text' id='about-text'>
         <h1>ABOUT ME</h1>
         <div className='text-section'>
-          <h6>This is a paragraph all about ME! Full Stack Software Developer with a passion for applying new technologies to enhance the professional and innovative atmosphere of an organization and working with dedicated teams to build efficient and robust applications. With a background in executive administration, I know how to prioritize competing tasks under pressure, meet deadlines, and deliver a quality product for the client.
+          <h6>I am a creative Software Engineer based in Dallas, TX with a passion for applying new cutting-edge technologies to enhance the innovative atmosphere of a business.
 
-          Ipsum occaecat ipsum pariatur dolore aute eu reprehenderit exercitation occaecat sint culpa mollit anim veniam. Deserunt amet incididunt commodo magna esse commodo ullamco. Ad amet nisi nisi mollit ullamco id et quis.</h6>
+            My interest in web development started in 2016 when I built my very first business website using custom themes and editing the html and css. After that, I created websites for family and friends then began as a freelancer part-time.
+
+            In the last 3 years, I've had the privilege of working as an Executive Admin Assistant where I worked hand-in-hand with the Software Engineering Department to review and give feedback for healthcare database structures and I designed custom marketing materials such as global weekly newsletters, email campaigns, and websites.
+
+            Fast-forward to today, my recently finished Full-Stack Software Engineering program experience with Flatiron School has shaped my coding knowledge with object-oriented programming, user interfaces, wireframes, Javascript, Ruby, Rails, among other areas. I’m excited to combine my experience in business administration and entrepreneurship with my software development skills!</h6>
           <br></br>
           <h4> Technical Skills</h4>
           <h6> Ruby, Javascript, HTML CSS Rails, Sinatra, React, Redux PostgreSQL, SQL</h6><br />
